10c74199f0c87158c98d07306e20f2406f4b520e
Mathew Cormier <mathew.corm@gmail.com>
2019-03-15 08:54:04 -0400

    getAbisInFolder

--- cli/abi.js
+++ cli/abi.js
@@ -1,10 +1,29 @@
 const importFresh = require('import-fresh')
+const { readdir } = require('fs-extra')
+const path = require('path')
  
 const getCallFunctions = abi => abi.filter(member => member.constant)
 
 
+/**
+ * Returns the ABIs from all truffle contract json files
+ * in a folder 
+ * // TODO: Recursive search
+ * // TODO: Validate path
+ * @param {string} folderPath 
+ */
+const getAbisInFolder = async folderPath => {
+    const files = await readdir(folderPath)
+    return files.map(file => getAbiFromFile(path.join(folderPath, file)))
+}
+
+/**
+ * Returns the ABI from a truffle contract json file
+ * // TODO: Load asynchronously
+ * @param {string} path 
+ */
 const getAbiFromFile = path => importFresh(path).abi
 
 
 
-module.exports = { getCallFunctions, getAbiFromFile }
+module.exports = { getCallFunctions, getAbiFromFile, getAbisInFolder }
--- cli/index.js
+++ cli/index.js
@@ -1,9 +1,13 @@
 const { abi } = require('../build/contracts/ERC20.json')
 const eblockClassTemplate = require('./templates/eblock-class')
-const { getAbiFromFile } = require('./abi')
+const { getAbiFromFile, getAbisInFolder } = require('./abi')
 const getPath = require('./get-path')
 
 //console.log(eblockClassTemplate({ name: 'ERC20', abi }))
-console.log(getAbiFromFile(getPath('./build/contracts/ERC20.json')))
+async function main() {
+    console.log(await getAbisInFolder(getPath('./build/contracts')))
+}
+
+main()
 
 
