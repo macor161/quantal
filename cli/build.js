30077fe92594a425667df168176dd2ad5f7874d0
Mathew Cormier <mathew.corm@gmail.com>
2019-03-20 14:48:10 -0400

    Add watch option

--- cli/build.js
+++ cli/build.js
@@ -4,42 +4,72 @@ const { generateJsFiles } = require('./generate-js-files')
 const compileContracts = require('./compile-contracts')
 const fs = require('fs')
 const path = require('path')
-const { throttle } = require('lodash')
-const options = getOptions()
 
-// Prevent multiple builds from running in parallel
-let isBuildRunning = false
 
-exports.build = throttle(async (options) => {    
+/**
+ * 
+ * @param {Object} opts Options
+ * @param {Function} opts.onComplete Called everytime a build is successfully completed
+ */
+module.exports = async function(options = {}) {
+    const buildFn = preventConcurentCalls(build)
+    
+    if (options.watch) {
+        fs.watch(getPath('contracts'), async (eventType, fileName) => {
+            if (path.extname(fileName).toLowerCase() !== '.sol')
+                return
+
+            console.log(`${eventType} file change: ${fileName}`)
+            await buildFn(options)
+        })
+    }
+
+    await buildFn(options)
+}
+
+/**
+ * 
+ * @param {Object} opts Options
+ */
+async function build(opts) {    
     try {
-        if (isBuildRunning) 
-            return
 
-        isBuildRunning = true
         console.log('Building')
+        const options = getOptions()
         await compileContracts(options)
         await generateJsFiles(options)
-        console.log('done')
+        
+        if (opts.onComplete)
+            opts.onComplete()
 
-        isBuildRunning = false
+        console.log('done')
+        return true
 
     } catch(err) {
         console.log('Error: ', err.message)
+        return false
     }
-}, options.throttle, { trailing: false })
+}
 
 
-
-exports.watch = async function() {
-    const { build } = exports
-    const options = getOptions()
+/**
+ * Prevent an async function to be called multiple times while still running
+ * @param {Function} fn  
+ */
+function preventConcurentCalls(fn) {
+    let isRunning = false
     
-    fs.watch(getPath('contracts'), async (eventType, fileName) => {
-        if (path.extname(fileName).toLowerCase() !== '.sol')
+    return async function(...args) {
+        if (isRunning)
             return
 
-        console.log(`${eventType} file change: ${fileName}`)
-        await build(options)
-    })
+        isRunning = true
+        const returnedValue = await fn(...args)
+        isRunning = false
+        
+        return returnedValue
+    }
 }
+
+
 
