4d2007a5ff8fda81ade640925c9f4864deb2cdf3
Mathew Cormier <mathew@Mathews-MacBook-Pro.local>
2019-06-04 16:02:07 -0400

    Add empty dependency-check test

---
+++ tests/dependency-check.spec.js
@@ -0,0 +1,8 @@
+const dependencyCheck = require('../src/dependency-management')()
+
+describe('dependency-management', () => {
+  test('', async () => {
+    // console.log(await dependencyCheck())
+    // TODO
+  }, 30000)
+})
