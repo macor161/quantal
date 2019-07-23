90d4ee28bb945324357282380028bf132ee6c686
Mathew Cormier <mathew.corm@gmail.com>
2019-07-23 00:04:23 -0400

    Remove onCompile function

--- src/compiler.js
+++ src/compiler.js
@@ -26,16 +26,11 @@ const defaultSelectors = {
 // Most basic of the compile commands. Takes a hash of sources, where
 // the keys are file or module paths and the values are the bodies of
 // the contracts. Does not evaulate dependencies that aren't already given.
-//
-// Default options:
-// {
-//   quiet: false,
-// }
-const compile = function(sources, options) {
+const compile = function(inputSources, options) {
   return new Promise(async (res,rej) => {
     const solcVersion = getOptions().compiler.version
     options.compilers.solc.version = solcVersion
-
+    const compilerInfo = { name: 'solc', version: getFormattedVersion(solcVersion) }
     const hasTargets = options.compilationTargets && options.compilationTargets.length
 
     expect.options(options, ['contracts_directory', 'compilers'])
@@ -67,7 +62,7 @@ const compile = function(sources, options) {
       operatingSystemIndependentSources,
       operatingSystemIndependentTargets,
       originalPathMappings
-    } = formatPaths(sources, hasTargets)
+    } = formatPaths(inputSources, hasTargets)
 
     // Specify compilation targets
     // Each target uses defaultSelectors, defaulting to single target `*` if targets are unspecified
@@ -86,7 +81,7 @@ const compile = function(sources, options) {
     }
 
     // Nothing to compile? Bail.
-    if (Object.keys(sources).length === 0) {
+    if (Object.keys(inputSources).length === 0) {
       return res({
         contracts: [], 
         files: {},
@@ -95,113 +90,105 @@ const compile = function(sources, options) {
       })
     }
 
-    const onCompiled = async (standardOutput) => {
-      debug('Compilation done')
-
-      const { contracts, sources, errors: allErrors = [] } = standardOutput
-      const warnings = allErrors.filter((error) => error.severity === 'warning')
-      const errors = allErrors.filter((error) => error.severity !== 'warning')      
-
-      if (errors.length > 0) 
-        return rej(await Promise.all(errors.map((err) => detailedError(err))))      
-
-      const files = []      
-      for (const [filePath, source] of Object.entries(sources))
-        files[source.id] = originalPathMappings[filePath]
-
-      const returnVal = {}
-      console.log('contracts: ', contracts)
-
-      Object.keys(contracts).forEach((source_path) => {
-        const files_contracts = contracts[source_path];
-
-        Object.keys(files_contracts).forEach((contract_name) => {
-          const contract = files_contracts[contract_name];
-          //console.log('contract ' + contract_name + ': ', contract)
-
-          // All source will have a key, but only the compiled source will have
-          // the evm output.
-          if (!Object.keys(contract.evm).length) return;
-
-          const contract_definition = {
-            contract_name: contract_name,
-            sourcePath: originalPathMappings[source_path], // Save original source path, not modified ones
-            source: operatingSystemIndependentSources[source_path],
-            sourceMap: contract.evm.bytecode.sourceMap,
-            deployedSourceMap: contract.evm.deployedBytecode.sourceMap,
-            legacyAST: sources[source_path].legacyAST,
-            ast: sources[source_path].ast,
-            abi: contract.abi,
-            metadata: contract.metadata,
-            bytecode: '0x' + contract.evm.bytecode.object,
-            deployedBytecode: '0x' + contract.evm.deployedBytecode.object,
-            unlinked_binary: '0x' + contract.evm.bytecode.object, // deprecated
-            compiler: {
-              name: 'solc',
-              version: getFormattedVersion(solcVersion),
-            },
-            devdoc: contract.devdoc,
-            userdoc: contract.userdoc,
-          };
-
-          // Reorder ABI so functions are listed in the order they appear
-          // in the source file. Solidity tests need to execute in their expected sequence.
-          contract_definition.abi = orderABI(contract_definition);
-
-          // Go through the link references and replace them with older-style
-          // identifiers. We'll do this until we're ready to making a breaking
-          // change to this code.
-          Object.keys(contract.evm.bytecode.linkReferences).forEach(file_name => {
-            const fileLinks = contract.evm.bytecode.linkReferences[file_name];
-
-            Object.keys(fileLinks).forEach(library_name => {
-              const linkReferences = fileLinks[library_name] || [];
-
-              contract_definition.bytecode = replaceLinkReferences(
-                  contract_definition.bytecode,
-                  linkReferences,
-                  library_name
-              );
-              contract_definition.unlinked_binary = replaceLinkReferences(
-                  contract_definition.unlinked_binary,
-                  linkReferences,
-                  library_name
-              );
-            });
-          });
-
-          // Now for the deployed bytecode
-          Object.keys(contract.evm.deployedBytecode.linkReferences).forEach(file_name => {
-                const fileLinks = contract.evm.deployedBytecode.linkReferences[file_name]
-
-                Object.keys(fileLinks).forEach(library_name => {
-                  const linkReferences = fileLinks[library_name] || [];
-
-                  contract_definition.deployedBytecode = replaceLinkReferences(
-                      contract_definition.deployedBytecode,
-                      linkReferences,
-                      library_name
-                  );
-                });
-              }
-          );
-
-          returnVal[contract_name] = contract_definition;
-        });
-      });
-
-      const compilerInfo = {name: 'solc', version: getFormattedVersion(solcVersion)}
-
-      Promise.all(warnings.map((warn) => detailedError(warn, standardOutput.sources[_.get(warn, ['sourceLocation', 'file'])])))
-          .then((warnings) => {
-            return res({ contracts: returnVal, files, compilerInfo, warnings })
-          })
-    }
-
-    
     debug('Starting compilation')
     const standardOutput = await compiler(operatingSystemIndependentSources, compilerSettings, options.compilers.solc.version)
-    onCompiled(standardOutput)
+    
+    debug('Compilation done')
+
+    const { contracts, sources, errors: allErrors = [] } = standardOutput
+    const warnings = allErrors.filter((error) => error.severity === 'warning')
+    const errors = allErrors.filter((error) => error.severity !== 'warning')      
+
+    if (errors.length > 0) 
+      return rej(await Promise.all(errors.map((err) => detailedError(err))))      
+
+    const files = []      
+    for (const [filePath, source] of Object.entries(sources))
+      files[source.id] = originalPathMappings[filePath]
+
+    const returnVal = {}
+
+    Object.keys(contracts).forEach((source_path) => {
+      const files_contracts = contracts[source_path];
+
+      Object.keys(files_contracts).forEach((contract_name) => {
+        const contract = files_contracts[contract_name];
+
+        // All source will have a key, but only the compiled source will have
+        // the evm output.
+        if (!Object.keys(contract.evm).length) 
+          return
+
+        const contract_definition = {
+          contract_name: contract_name,
+          sourcePath: originalPathMappings[source_path], // Save original source path, not modified ones
+          source: operatingSystemIndependentSources[source_path],
+          sourceMap: contract.evm.bytecode.sourceMap,
+          deployedSourceMap: contract.evm.deployedBytecode.sourceMap,
+          legacyAST: sources[source_path].legacyAST,
+          ast: sources[source_path].ast,
+          abi: contract.abi,
+          metadata: contract.metadata,
+          bytecode: '0x' + contract.evm.bytecode.object,
+          deployedBytecode: '0x' + contract.evm.deployedBytecode.object,
+          unlinked_binary: '0x' + contract.evm.bytecode.object, // deprecated
+          compiler: compilerInfo,
+          devdoc: contract.devdoc,
+          userdoc: contract.userdoc
+        }
+
+        // Reorder ABI so functions are listed in the order they appear
+        // in the source file. Solidity tests need to execute in their expected sequence.
+        contract_definition.abi = orderABI(contract_definition);
+
+        // Go through the link references and replace them with older-style
+        // identifiers. We'll do this until we're ready to making a breaking
+        // change to this code.
+        Object.keys(contract.evm.bytecode.linkReferences).forEach(file_name => {
+          const fileLinks = contract.evm.bytecode.linkReferences[file_name];
+
+          Object.keys(fileLinks).forEach(library_name => {
+            const linkReferences = fileLinks[library_name] || [];
+
+            contract_definition.bytecode = replaceLinkReferences(
+                contract_definition.bytecode,
+                linkReferences,
+                library_name
+            );
+            contract_definition.unlinked_binary = replaceLinkReferences(
+                contract_definition.unlinked_binary,
+                linkReferences,
+                library_name
+            );
+          });
+        });
+
+        // Now for the deployed bytecode
+        Object.keys(contract.evm.deployedBytecode.linkReferences).forEach(file_name => {
+              const fileLinks = contract.evm.deployedBytecode.linkReferences[file_name]
+
+              Object.keys(fileLinks).forEach(library_name => {
+                const linkReferences = fileLinks[library_name] || [];
+
+                contract_definition.deployedBytecode = replaceLinkReferences(
+                    contract_definition.deployedBytecode,
+                    linkReferences,
+                    library_name
+                );
+              });
+            }
+        );
+
+        returnVal[contract_name] = contract_definition
+      })
+    })
+
+    
+    Promise.all(warnings.map((warn) => detailedError(warn, standardOutput.sources[_.get(warn, ['sourceLocation', 'file'])])))
+        .then((warnings) => {
+          return res({ contracts: returnVal, files, compilerInfo, warnings })
+        })
+    
 
   })
 }
@@ -333,7 +320,6 @@ function orderABI(contract) {
 }
 
 // contracts_directory: String. Directory where .sol files can be found.
-// quiet: Boolean. Suppress output. Defaults to false.
 compile.all = function(options) {
   return new Promise((res, rej) => { 
     debug('compile.all started')
@@ -349,13 +335,12 @@ compile.all = function(options) {
 // build_directory: String. Optional. Directory where .sol.js files can be found. Only required if `all` is false.
 // all: Boolean. Compile all sources found. Defaults to true. If false, will compare sources against built files
 //      in the build directory to see what needs to be compiled.
-// quiet: Boolean. Suppress output. Defaults to false.
 compile.necessary = function(options) {
   return new Promise((res, rej) => { 
     Profiler.updated(options, function(err, updated) {
       if (err) return rej(err)
 
-      if (updated.length === 0 && options.quiet !== true) {
+      if (updated.length === 0) {
         return res({
           contracts: [], 
           files: {},
