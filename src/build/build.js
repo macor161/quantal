bf0447cf54c03c326ef59498670cacfcd6db2316
Mathew Cormier <mathew.corm@gmail.com>
2019-08-11 19:52:46 -0400

    Resolver options

--- src/build/build.js
+++ src/build/build.js
@@ -20,7 +20,7 @@ async function build(options) {
   const warningCache = new WarningCache({ builtContractsDir: options.builtContractsDir })
   await preloadCompiler(options.compiler.version)
   const truffleOptions = getTruffleOptions()
-  truffleOptions.resolver = new Resolver(truffleOptions)
+  truffleOptions.resolver = new Resolver({ cwd: options.cwd, buildDir: options.builtContractsDir })
 
   const compilation = await compileSources(options, truffleOptions)
   const allWarnings = await warningCache.updateCache({ contracts: compilation.contracts, warnings: compilation.warnings })
