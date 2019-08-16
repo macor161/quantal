ba4aec76aa8284b1f2d662c07082f420f3734c5c
Mathew Cormier <mathew.corm@gmail.com>
2019-08-16 15:44:20 -0400

    Update synthetix bytecode comparison test

--- test/test-projects/bytecode.spec.js
+++ test/test-projects/bytecode.spec.js
@@ -1,17 +1,34 @@
+const path = require('path')
+const requireAll = require('require-all')
 const { build } = require('../../src/build/build')
 const { getOptions } = require('../../src/options')
 
-describe('info', () => {
-  test('', async () => {
-    // const config = TruffleConfig.detect({ working_directory: './test/test-projects/synthetix' })
-    // const options = getOptions({ cwd: './test/test-projects/synthesia' })
-    // console.log('options: ', config.build_directory)
 
-    const options = getOptions({ cwd: './test/test-projects/synthetix' })
-    // console.log('cwd: ', process.cwd())
-    console.log('options: ', options)
+describe('bytecode comparison', () => {
+  test('synthetix', async () => {
+    const PATH = path.join(__dirname, 'synthetix')
+    const options = getOptions({ cwd: PATH })
 
     const result = await build(options)
-    console.log(result)
+    const truffleContracts = requireAll(path.join(PATH, 'build', 'contracts'))
+
+    for (const [contractName, artifact] of Object.entries(result.contracts)) {
+      const truffleContract = findContract(contractName, truffleContracts)
+
+      if (!truffleContract)
+        throw new Error(`Cannot find truffle contract ${contractName}`)
+
+      if (artifact.bytecode !== truffleContract.metadata) {
+        console.error(`Bytecode mismatch for contract ${contractName}`)
+        expect(artifact.bytecode).toEqual(truffleContract.metadata)
+      }
+    }
   }, 30000)
 })
+
+function findContract(contractName, contracts) {
+  for (const [, artifact] of Object.entries(contracts)) {
+    if (artifact.contractName === contractName)
+      return artifact
+  }
+}
