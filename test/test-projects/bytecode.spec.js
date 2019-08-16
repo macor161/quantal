43a618997f9e57beb1e48a3543f0157e434964e6
Mathew Cormier <mathew.corm@gmail.com>
2019-08-16 16:33:08 -0400

    openzeppelin bytecode comparison

--- test/test-projects/bytecode.spec.js
+++ test/test-projects/bytecode.spec.js
@@ -5,26 +5,40 @@ const { getOptions } = require('../../src/options')
 
 
 describe('bytecode comparison', () => {
+  test('openzeppelin', async () => {
+    const PATH = path.join(__dirname, 'openzeppelin-solidity')
+    const options = getOptions({ cwd: PATH, noCache: true })
+
+    const result = await build(options)
+    const truffleContracts = requireAll(path.join(PATH, 'truffle-build', 'contracts'))
+
+    compareContracts(result.contracts, truffleContracts)
+  }, 30000)
+
   test('synthetix', async () => {
     const PATH = path.join(__dirname, 'synthetix')
     const options = getOptions({ cwd: PATH, noCache: true })
 
     const result = await build(options)
-    const truffleContracts = requireAll(path.join(PATH, 'build', 'contracts'))
+    const truffleContracts = requireAll(path.join(PATH, 'truffle-build', 'contracts'))
 
-    for (const [contractName, artifact] of Object.entries(result.contracts)) {
-      const truffleContract = findContract(contractName, truffleContracts)
-
-      if (!truffleContract)
-        throw new Error(`Cannot find truffle contract ${contractName}`)
-
-      if (artifact.bytecode !== truffleContract.bytecode) {
-        console.error(`Bytecode mismatch for contract ${contractName}`)
-        expect(artifact.bytecode).toEqual(truffleContract.bytecode)
-      }
-    }
+    compareContracts(result.contracts, truffleContracts)
   }, 30000)
 })
+
+function compareContracts(contracts, truffleContracts) {
+  for (const [contractName, artifact] of Object.entries(contracts)) {
+    const truffleContract = findContract(contractName, truffleContracts)
+
+    if (!truffleContract)
+      throw new Error(`Cannot find truffle contract ${contractName}`)
+
+    if (artifact.bytecode !== truffleContract.bytecode) {
+      console.error(`Bytecode mismatch for contract ${contractName}`)
+      expect(artifact.bytecode).toEqual(truffleContract.bytecode)
+    }
+  }
+}
 
 function findContract(contractName, contracts) {
   for (const [, artifact] of Object.entries(contracts)) {
