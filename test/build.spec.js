38f62cae5a9f68eae9bf4172abc1cf132447a49f
Mathew Cormier <mathew.corm@gmail.com>
2019-08-21 11:38:56 -0400

    Add empty build test

---
+++ test/build.spec.js
@@ -0,0 +1,10 @@
+const path = require('path')
+const requireAll = require('require-all')
+const execa = require('execa')
+const { build, getOptions } = require('../src/build/build')
+
+
+describe('build', () => {
+
+
+})
--- test/formatting.spec.js
+++ test/formatting.spec.js
@@ -3,19 +3,18 @@ const { formatSource } = require('../src/formatting')
 
 
 function getMockedColors() {
-    return {
-        controls: jest.fn(text => text), 
-        declarations: jest.fn(text => text), 
-        types: jest.fn(text => text), 
-        comments: jest.fn(text => text), 
-        lineNumbers: jest.fn(text => text)         
-    }
+  return {
+    controls: jest.fn(text => text),
+    declarations: jest.fn(text => text),
+    types: jest.fn(text => text),
+    comments: jest.fn(text => text),
+    lineNumbers: jest.fn(text => text),
+  }
 }
 
 describe('Fromatting', () => {
-    test('Format types', async () => {  
-        
-        const source = outdent`
+  test('Format types', async () => {
+    const source = outdent`
         contract Test is Test2 {
             uint256 public s3;
           
@@ -31,25 +30,24 @@ describe('Fromatting', () => {
             }
         }`
 
-        const colors = getMockedColors()
-        
-        formatSource(source.split('\n'), { colors })
+    const colors = getMockedColors()
 
-        expect(colors.types.mock.calls.length).toBe(9)
-        expect(colors.types.mock.calls[0][0]).toBe('uint256')
-        expect(colors.types.mock.calls[1][0]).toBe('bytes32')
-        expect(colors.types.mock.calls[2][0]).toBe('address')
-        expect(colors.types.mock.calls[3][0]).toBe('string')
-        expect(colors.types.mock.calls[4][0]).toBe('bytes32')
-        expect(colors.types.mock.calls[5][0]).toBe('address')
-        expect(colors.types.mock.calls[6][0]).toBe('bool')
-        expect(colors.types.mock.calls[7][0]).toBe('address')
-        expect(colors.types.mock.calls[8][0]).toBe('int')
-    })
+    formatSource(source.split('\n'), { colors })
 
-    test('Format declarations', async () => {  
-        
-        const source = outdent`
+    expect(colors.types.mock.calls.length).toBe(9)
+    expect(colors.types.mock.calls[0][0]).toBe('uint256')
+    expect(colors.types.mock.calls[1][0]).toBe('bytes32')
+    expect(colors.types.mock.calls[2][0]).toBe('address')
+    expect(colors.types.mock.calls[3][0]).toBe('string')
+    expect(colors.types.mock.calls[4][0]).toBe('bytes32')
+    expect(colors.types.mock.calls[5][0]).toBe('address')
+    expect(colors.types.mock.calls[6][0]).toBe('bool')
+    expect(colors.types.mock.calls[7][0]).toBe('address')
+    expect(colors.types.mock.calls[8][0]).toBe('int')
+  })
+
+  test('Format declarations', async () => {
+    const source = outdent`
         pragma solidity ^0.5.0;
         import "./Test2.sol";
 
@@ -65,27 +63,26 @@ describe('Fromatting', () => {
             }
         }`
 
-        const colors = getMockedColors()
-        
-        formatSource(source.split('\n'), { colors })
+    const colors = getMockedColors()
 
-        expect(colors.declarations.mock.calls.length).toBe(9)
-        
-        expect(colors.declarations.mock.calls[0][0]).toBe('solidity')
-        expect(colors.declarations.mock.calls[1][0]).toBe('contract')
-        expect(colors.declarations.mock.calls[2][0]).toBe('is')
-        expect(colors.declarations.mock.calls[3][0]).toBe('constructor')
-        expect(colors.declarations.mock.calls[4][0]).toBe('public')
-        expect(colors.declarations.mock.calls[5][0]).toBe('false')
-        expect(colors.declarations.mock.calls[6][0]).toBe('function')
-        expect(colors.declarations.mock.calls[7][0]).toBe('private')
-        expect(colors.declarations.mock.calls[8][0]).toBe('true')
-    })
+    formatSource(source.split('\n'), { colors })
+
+    expect(colors.declarations.mock.calls.length).toBe(9)
+
+    expect(colors.declarations.mock.calls[0][0]).toBe('solidity')
+    expect(colors.declarations.mock.calls[1][0]).toBe('contract')
+    expect(colors.declarations.mock.calls[2][0]).toBe('is')
+    expect(colors.declarations.mock.calls[3][0]).toBe('constructor')
+    expect(colors.declarations.mock.calls[4][0]).toBe('public')
+    expect(colors.declarations.mock.calls[5][0]).toBe('false')
+    expect(colors.declarations.mock.calls[6][0]).toBe('function')
+    expect(colors.declarations.mock.calls[7][0]).toBe('private')
+    expect(colors.declarations.mock.calls[8][0]).toBe('true')
+  })
 
 
-    test('Format controls', async () => {  
-        
-        const source = outdent`
+  test('Format controls', async () => {
+    const source = outdent`
         pragma solidity ^0.5.0;
         import "./Test2.sol";
 
@@ -103,39 +100,35 @@ describe('Fromatting', () => {
             }
         }`
 
-        const colors = getMockedColors()
-        
-        formatSource(source.split('\n'), { colors })
+    const colors = getMockedColors()
 
-        expect(colors.controls.mock.calls.length).toBe(6)
-        
-        expect(colors.controls.mock.calls[0][0]).toBe('pragma')
-        expect(colors.controls.mock.calls[1][0]).toBe('import')
-        expect(colors.controls.mock.calls[2][0]).toBe('require')
-        expect(colors.controls.mock.calls[3][0]).toBe('returns')
-        expect(colors.controls.mock.calls[4][0]).toBe('emit')
-        expect(colors.controls.mock.calls[5][0]).toBe('return')
-    })    
+    formatSource(source.split('\n'), { colors })
 
-    
-    test('Format single line comments', async () => {  
-        
-        const source = outdent`
+    expect(colors.controls.mock.calls.length).toBe(6)
+
+    expect(colors.controls.mock.calls[0][0]).toBe('pragma')
+    expect(colors.controls.mock.calls[1][0]).toBe('import')
+    expect(colors.controls.mock.calls[2][0]).toBe('require')
+    expect(colors.controls.mock.calls[3][0]).toBe('returns')
+    expect(colors.controls.mock.calls[4][0]).toBe('emit')
+    expect(colors.controls.mock.calls[5][0]).toBe('return')
+  })
+
+
+  test('Format single line comments', async () => {
+    const source = outdent`
         // This is a test
         function setS(uint256 _value) public returns(uint256) {
           s = s + _value; // This is another test
           return s;
         } `
 
-        const colors = getMockedColors()
-        
-        formatSource(source.split('\n'), { colors })
+    const colors = getMockedColors()
 
-        expect(colors.comments.mock.calls.length).toBe(2)
-        expect(colors.comments.mock.calls[0][0]).toBe('// This is a test')
-        expect(colors.comments.mock.calls[1][0]).toBe('// This is another test')
+    formatSource(source.split('\n'), { colors })
 
-    })
-
+    expect(colors.comments.mock.calls.length).toBe(2)
+    expect(colors.comments.mock.calls[0][0]).toBe('// This is a test')
+    expect(colors.comments.mock.calls[1][0]).toBe('// This is another test')
+  })
 })
-
